35308dc053464a2f593a741f1df8b3717c0b9f90
Saberslay <dansteamshell@gmail.com>
2022-06-05 22:44:53 +0100

    Init

---
+++ Utils/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils{
+    public class Logger {
+        public static void LogError(String s) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(s);
+        }
+        public static void LogInfo(String s) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(s);
+        }
+        public static void ReedKey() {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Utils/Speech.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace Utils {
+    public class Speech {
+        private static SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        public static void Speak_Neutral(string message) {
+            synth.SelectVoiceByHints(VoiceGender.Neutral);
+            synth.Speak(message);
+        }
+        public static void Speak_Female(string message) {
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.Speak(message);
+        }
+        public static void Speak_Male(string message) {
+            synth.SelectVoiceByHints(VoiceGender.Male);
+            synth.Speak(message);
+        }
+        public static void Speak_NotSet(string message) {
+            synth.SelectVoiceByHints(VoiceGender.NotSet);
+            synth.Speak(message);
+        }
+    }
+}
