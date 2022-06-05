3ca50cb3fa7e53b356c605ee445ce710034c9e70
Saberslay <dansteamshell@gmail.com>
2022-06-06 00:58:51 +0100

    Added Message Bot popups

--- Utils/Logger.cs
+++ Utils/Logger.cs
@@ -4,14 +4,15 @@ namespace Utils{
     public class Logger {
         public static void LogError(String s) {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{0}{1}", DateTime.Now, s);
-        }public static void LogWanning(String s) {
+            Console.WriteLine($"[{0}] {1}", DateTime.Now, s);
+        }
+        public static void LogWanning(String s) {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"{0}{1}",DateTime.Now,s);
+            Console.WriteLine($"[{0}] {1}",DateTime.Now,s);
         }
         public static void LogInfo(String s) {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine($"{0}{1}", DateTime.Now, s);
+            Console.WriteLine($"[{0}] {1}", DateTime.Now, s);
         }
         public static void ReedKey() {
             Console.ReadKey();
---
+++ Utils/Windows/Forms.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Forms;
+
+namespace Utils.Windows {
+    public class Forms {
+        #region Message Box Error
+            public static void MessageBox_Error_OK(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            public static void MessageBox_Error_OK_Cancel(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            public static void MessageBox_Error_Yes_No_Cancel(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            }
+            public static void MessageBox_Error_Retry_Cancel(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            public static void MessageBox_Error_Yes_No(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            }
+            public static void MessageBox_Error_Abort_Retry_Ignore(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            }
+        #endregion
+
+        #region Message Box Warning
+            public static void MessageBox_Warning_OK(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            public static void MessageBox_Warning_OK_Cancel(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+            public static void MessageBox_Warning_Yes_No_Cancel(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            }
+            public static void MessageBox_Warning_Retry_Cancel(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+            }
+            public static void MessageBox_Warning_Yes_No(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            public static void MessageBox_Warning_Abort_Retry_Ignore(string Title, string Message) {
+                MessageBox.Show(Message, Title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Warning);
+            }
+        #endregion
+
+        #region Message Box Information
+        public static void MessageBox_Information_OK(string Title, string Message) {
+            MessageBox.Show(Message, Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public static void MessageBox_Information_OK_Cancel(string Title, string Message) {
+            MessageBox.Show(Message, Title, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+        public static void MessageBox_Information_Yes_No_Cancel(string Title, string Message) {
+            MessageBox.Show(Message, Title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+        }
+        public static void MessageBox_Information_Retry_Cancel(string Title, string Message) {
+            MessageBox.Show(Message, Title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+        }
+        public static void MessageBox_Information_Yes_No(string Title, string Message) {
+            MessageBox.Show(Message, Title, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        }
+        public static void MessageBox_Information_Abort_Retry_Ignore(string Title, string Message) {
+            MessageBox.Show(Message, Title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Information);
+        }
+        #endregion
+    }
+}
