7ed0f0572b0efdd74de16e95420aec4f64271a28
Saberslay <dansteamshell@gmail.com>
2023-01-26 23:49:27 +0000

    refactored some namespaces

--- Utils/Speech.cs
+++ Utils/Speech.cs
@@ -3,20 +3,19 @@
 namespace Utils {
     public class Speech {
         private static SpeechSynthesizer synth = new SpeechSynthesizer();
-
-        public static void Speak_Neutral(string message) {
+        public static void Speak_VoiceGender_Neutral(string message) {
             synth.SelectVoiceByHints(VoiceGender.Neutral);
             synth.Speak(message);
         }
-        public static void Speak_Female(string message) {
+        public static void Speak_VoiceGender_Female(string message) {
             synth.SelectVoiceByHints(VoiceGender.Female);
             synth.Speak(message);
         }
-        public static void Speak_Male(string message) {
+        public static void Speak_VoiceGender_Male(string message) {
             synth.SelectVoiceByHints(VoiceGender.Male);
             synth.Speak(message);
         }
-        public static void Speak_NotSet(string message) {
+        public static void Speak_VoiceGender_NotSet(string message) {
             synth.SelectVoiceByHints(VoiceGender.NotSet);
             synth.Speak(message);
         }
